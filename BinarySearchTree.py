32f1c7e3e34f5eba7f5c39c2229e9f581ac0bdef
Shivaditya Meduri <77324692+shivaditya-meduri@users.noreply.github.com>
2025-01-11 11:24:13 +0100

    Create BinarySearchTree.py

---
+++ BinarySearchTree.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else: 
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        temp = self.root
+        while temp is not None:
+            if value == temp.value:
+                return True 
+            elif value > temp.value:
+                temp = temp.right 
+            else:
+                temp = temp.left 
+        return False
+
+# Example usage of the above class
+bst = BinarySearchTree()
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+result = bst.contains(10)
